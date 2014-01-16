c2f3d2665edfa4c2653adacb7ccf7800546453fe
chadcolby <chad.david.colby@gmail.com>
2014-01-16 13:55:43 -0800

    trying to add project file

---
+++ ClassRoster/FKAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FKAAppDelegate.h
+//  ClassRoster
+//
+//  Created by Chad D Colby on 1/14/14.
+//  Copyright (c) 2014 kramerica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FKAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassRoster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClassRoster
+//
+//  Created by Chad D Colby on 1/14/14.
+//  Copyright (c) 2014 kramerica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FKAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FKAAppDelegate class]));
+    }
+}
